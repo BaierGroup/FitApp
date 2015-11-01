5fc896bca03a007ff198fc48c7c3fb56c6a0e857
fjhjc01 <fjhjc01@bu.edu>
2015-11-01 09:03:50 -0500

    finish first data

--- FitAppMS/public/stylesheets/js/plugins/flot/flot-data.js
+++ FitAppMS/public/stylesheets/js/plugins/flot/flot-data.js
@@ -68,25 +68,25 @@ $(function() {
         data: 20
     }];
 
-    var plotObj = $.plot($("#flot-pie-chart"), data, {
-        series: {
-            pie: {
-                show: true
-            }
-        },
-        grid: {
-            hoverable: true
-        },
-        tooltip: true,
-        tooltipOpts: {
-            content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
-            shifts: {
-                x: 20,
-                y: 0
-            },
-            defaultTheme: false
-        }
-    });
+    // var plotObj = $.plot($("#flot-pie-chart"), data, {
+    //     series: {
+    //         pie: {
+    //             show: true
+    //         }
+    //     },
+    //     grid: {
+    //         hoverable: true
+    //     },
+    //     tooltip: true,
+    //     tooltipOpts: {
+    //         content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
+    //         shifts: {
+    //             x: 20,
+    //             y: 0
+    //         },
+    //         defaultTheme: false
+    //     }
+    // });
 
 });
 
